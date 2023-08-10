3ed7af6eed5c056590fd317c8a27b420679ef03d
Jackie <1824561218@qq.com>
2023-08-10 20:22:29 +0800

    scroll滚动-自动滚动组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,40 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+/*
+ * @Author: Jackie
+ * @Date: 2023-06-25 09:58:10
+ * @LastEditTime: 2023-08-10 20:21:58
+ * @LastEditors: Jackie
+ * @Description: router
+ * @FilePath: /vue3-swiper-demo/src/router/index.js
+ * @version:
+ */
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
+  {
+    path: '/scroll',
+    name: 'scroll',
+    component: () => import('../views/ScrollView.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/ScrollView.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: Jackie
+ * @Date: 2023-08-10 20:20:55
+ * @LastEditTime: 2023-08-10 20:20:58
+ * @LastEditors: Jackie
+ * @Description: scroll
+ * @FilePath: /vue3-swiper-demo/src/views/ScrollView.vue
+ * @version: 
+-->
+<template>
+  <div class="scroll">
+    <Scroll4 />
+  </div>
+</template>
+
+<script setup>
+import Scroll4 from '@/components/scroll/Scroll4.vue';
+</script>
+
+<style></style>
